d623a67209dca24538339751b942c93a795aff5f
Bigkuma <s6506022420037@email.kmutnb.ac.th>
2023-08-15 11:06:30 +0700

    ssss

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT||3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello BIGKUMA");
 });
 
 app.listen(port, () => {
